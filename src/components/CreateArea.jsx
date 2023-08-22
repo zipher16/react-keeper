241ef3b2dbb2dea4c1823a8520419bf9306362e3
zipher16 <darsh.tech.2023@cyberservices.com>
2023-08-22 13:27:34 +0100

    Updated Styling

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
+import Fab from "@mui/material/Fab";
+import AddIcon from "@mui/icons-material/Add";
+import Zoom from "@mui/material/Zoom";
 
 function CreateArea(props) {
+	const [isExpanded, setExpanded] = useState(false);
+
 	const [note, setNote] = useState({
 		title: "",
 		content: "",
@@ -24,24 +29,40 @@ function CreateArea(props) {
 		});
 		event.preventDefault();
 	}
+	function expand() {
+		setExpanded(true);
+	}
 
 	return (
 		<div>
 			<form className="create-note">
-				<input
-					name="title"
-					onChange={handleChange}
-					value={note.title}
-					placeholder="Title"
-				/>
+				{isExpanded && (
+					<input
+						name="title"
+						onChange={handleChange}
+						value={note.title}
+						placeholder="Title"
+					/>
+				)}
 				<textarea
 					name="content"
+					onClick={expand}
 					onChange={handleChange}
 					value={note.content}
 					placeholder="Take a note..."
-					rows="3"
+					rows={isExpanded ? 3 : 1}
 				/>
-				<button onClick={submitNote}>Add</button>
+				<Zoom in={isExpanded}>
+					<Fab
+						aria-label="add"
+						onClick={submitNote}
+					>
+						<AddIcon />
+					</Fab>
+				</Zoom>
+				{/* <button onClick={submitNote}>
+					<AddIcon />
+				</button> */}
 			</form>
 		</div>
 	);
